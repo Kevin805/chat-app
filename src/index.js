bace8e3ac6f795d1a3343aaea1801893508f02a7
Kevin805 <kevin.michael805@gmail.com>
2021-01-26 21:46:45 -0700

    signup and login complete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as ServiceWorker from './serviceWorker';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import LoginComponent from './login/login'
+import SignupComponent from './signup/signup'
+import DashboardComponent from './dashboard/dashboard'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBgxYoqiRPJTprUDs6REhPfNSfqKOp6qUE",
+  authDomain: "chat-app-9ced7.firebaseapp.com",
+  projectId: "chat-app-9ced7",
+  storageBucket: "chat-app-9ced7.appspot.com",
+  messagingSenderId: "417130787562",
+  appId: "1:417130787562:web:df69fcf18bcf66e11edd93",
+  measurementId: "G-QHL8CZZXW6"
+});
+
+const routing = (
+  <Router>
+    <div id='routing-container'>
+  <Route path='/login' component={LoginComponent}></Route>
+  <Route path='/signup' component={SignupComponent}></Route>
+  <Route path='/dashboard' component={DashboardComponent}></Route>
+    </div>
+  </Router>
+)
+
+
+ReactDOM.render( routing, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ServiceWorker.unregister();
